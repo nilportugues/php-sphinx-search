37e5c34aadc30f24344360d6fbe125a20cf8a32e
nilopc <nilopc@gmail.com>
2013-07-18 01:57:13 +0200

    Adding PHPUnit config

---
+++ src/NilPortugues/Sphinx/Tests/SphinxClientTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class SphinxClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRemoveFilter()
+    {
+
+    }
+}
---
+++ src/NilPortugues/Sphinx/Tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+// Sphinx connection parameters
+define('SPHINX_HOST', '127.0.0.1');
+define('SPHINX_PORT', 9312);
+
+// Redis connection parameters
+define('REDIS_HOST', '127.0.0.1');
+define('REDIS_PORT', 6379);
+
+// Memcached connection parameters
+define('MEMCACHE_HOST', '127.0.0.1');
+define('MEMCACHE_PORT', 11211);
