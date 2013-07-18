c4ca98723da84646d864e6d7609d80b02ba2fcaf
nilopc <nilopc@gmail.com>
2013-07-18 15:22:04 +0200

    Preparing test cases

--- src/NilPortugues/Sphinx/Tests/SphinxClientTest.php
+++ src/NilPortugues/Sphinx/Tests/SphinxClientTest.php
@@ -6,4 +6,179 @@ class SphinxClientTest extends \PHPUnit_Framework_TestCase
     {
 
     }
+
+    public function testGetLastError()
+    {
+
+    }
+
+    public function testGetLastWarning()
+    {
+
+    }
+
+    public function testIsConnectError()
+    {
+
+    }
+
+    public function testSetServer()
+    {
+
+    }
+
+    public function testSetConnectTimeout()
+    {
+
+    }
+
+    public function testSetLimits()
+    {
+
+    }
+
+    public function testSetMaxQueryTime()
+    {
+
+    }
+
+    public function testSetMatchMode()
+    {
+
+    }
+
+    public function testSetRankingMode()
+    {
+
+    }
+
+    public function testSetSortMode()
+    {
+
+    }
+
+    public function testSetWeights()
+    {
+
+    }
+
+    public function testSetFieldWeights()
+    {
+
+    }
+
+    public function testSetIndexWeights()
+    {
+
+    }
+
+    public function testSetIDRange()
+    {
+
+    }
+
+    public function testSetFilter()
+    {
+
+    }
+
+    public function testSetFilterRange()
+    {
+
+    }
+
+    public function testSetFilterFloatRange()
+    {
+
+    }
+
+    public function testSetGeoAnchor()
+    {
+
+    }
+
+    public function testSetGroupBy()
+    {
+
+    }
+
+    public function testSetGroupDistinct()
+    {
+
+    }
+
+    public function testSetRetries()
+    {
+
+    }
+
+    public function testSetArrayResult()
+    {
+
+    }
+
+    public function testSetOverride()
+    {
+
+    }
+
+    public function testSetSelect()
+    {
+
+    }
+
+    public function testResetFilters()
+    {
+
+    }
+
+    public function testResetGroupBy()
+    {
+
+    }
+
+    public function testResetOverrides()
+    {
+
+    }
+
+    public function testAddQuery()
+    {
+
+    }
+
+    public function testRunQueries()
+    {
+
+    }
+
+    public function testBuildExcerpts()
+    {
+
+    }
+
+    public function testBuildKeywords()
+    {
+
+    }
+
+    public function testEscapeString()
+    {
+
+    }
+
+    public function testUpdateAttributes()
+    {
+
+    }
+
+    public function testStatus()
+    {
+
+    }
+
+    public function testFlushAttributes()
+    {
+
+    }
 }
